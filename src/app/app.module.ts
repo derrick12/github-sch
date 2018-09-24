bf6e02112733f8dda6fd9eef775bc0c4630c3b59
derrick12 <derrickwaweru@gmail.com>
2018-09-24 16:57:14 +0300

    add service to providers and import Http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileService } from './services/profile.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HttpModule
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProfileService], // make it available throught the app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
