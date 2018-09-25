cf4e91da483cacf88bea4f93382caecee1892129
derrick12 <derrickwaweru@gmail.com>
2018-09-26 01:25:39 +0300

    add clientId and secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com",
+  clientId:"17338b5897f513344064",
+  clientSecret: "8975a02feb0e55cd4e6b73c54fb46e59fca3a7b6"
 };
