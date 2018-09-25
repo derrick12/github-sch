f7c3e296ba6e388e34071ef93781b93020a0429c
derrick12 <derrickwaweru@gmail.com>
2018-09-25 17:31:14 +0300

    add user properties

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,4 @@
+// User blueprint
 export class User {
-    constructor(public user: string) { }
+    constructor(public user: string, public user_login: string, public img_link: string, public user_link: string, public user_bio: string) { }
 }
