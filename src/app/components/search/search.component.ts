87197f9d3bd0b894599babc15c2cf22ba5f1a720
derrick12 <derrickwaweru@gmail.com>
2018-09-26 01:29:49 +0300

    inject service and add search user function

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,14 +1,29 @@
+import { User } from './../../user';
+import { ProfileService } from './../../services/profile.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
+  providers: [ProfileService],
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  username: string;
+  repos: any[];
 
+  constructor(private profileService: ProfileService) { }
+
+  searchUser() {
+    this.user = this.profileService.user;
+    this.profileService.getProfileData();
+    this.profileService.updateProfileData(this.username);
+    this.profileService.getRepoData();
+    this.repos = this.profileService.repoArray;
+  }
   ngOnInit() {
   }
 
