a3825a494dbb34e0de5602a23f8ac3f8ab9458c8
derrick12 <derrickwaweru@gmail.com>
2018-09-26 01:31:36 +0300

    add provider and define  profile properties

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,13 +1,28 @@
+import { ProfileService } from './../../services/profile.service';
+import { User } from './../../user';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  providers: [ProfileService],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile: any[];
+  user: User;
 
-  constructor() { }
+  // inject the service
+  constructor(private profileService: ProfileService) {
+
+    // call the functions
+    this.profileService.getMyProfile();
+    this.user = this.profileService.user;
+
+    this.profileService.getMyRepo();
+    this.profile = this.profileService.repoArray;
+}
 
   ngOnInit() {
   }
