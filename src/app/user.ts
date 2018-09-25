0b5c1544275b8aafcd313561070318da94f1bae2
derrick12 <derrickwaweru@gmail.com>
2018-09-26 01:33:05 +0300

    add repository blue print

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,5 @@
 // User blueprint
 export class User {
+    // tslint:disable-next-line:max-line-length
     constructor(public user: string, public user_login: string, public img_link: string, public user_link: string, public user_bio: string) { }
 }
