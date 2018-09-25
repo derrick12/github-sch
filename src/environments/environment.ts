cafa64d742181ed01e24b0ad5178ad1302430c06
derrick12 <derrickwaweru@gmail.com>
2018-09-26 01:26:12 +0300

    add client id and secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"https://api.github.com",
+  clientId:"17338b5897f513344064",
+  clientSecret: "8975a02feb0e55cd4e6b73c54fb46e59fca3a7b6"
 };
 
 /*
