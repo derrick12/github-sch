da3f4d76f37444a94dfd67509873045d19f9991d
derrick12 <derrickwaweru@gmail.com>
2018-09-26 01:34:21 +0300

    add routes and form imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileService } from './services/profile.service';
 import { SearchComponent } from './components/search/search.component';
+
+// Defining routes
+const routes:Routes = [
+  {path: "home", component: ProfileComponent},
+  {path: "search", component: SearchComponent},
+  {path: "", redirectTo: "/home", pathMatch: "full"}
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +24,9 @@ import { SearchComponent } from './components/search/search.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProfileService], // make it available throught the app
   bootstrap: [AppComponent]
